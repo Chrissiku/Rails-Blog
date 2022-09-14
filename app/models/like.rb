05f9e3a32ea4fad3cafa4c14beb5599df1e42050
chrissiku <chrissiku5@gmail.com>
2022-09-14 23:28:37 +0200

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
-class  Like < ApplicationRecord
-    belongs_to :user, class_name: "User", foreign_key: "AuthorId"
-    belongs_to :post, class_name: "Post", foreign_key: "PostId"
+class Like < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'AuthorId'
+  belongs_to :post, class_name: 'Post', foreign_key: 'PostId'
 
-    after_save :update_counter
+  after_save :update_counter
 
-    def update_counter
-        post.increment!(:LikesCounter)
-    end
+  def update_counter
+    post.increment!(:LikesCounter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-    has_many :posts, class_name: "Post", foreign_key: "AuthorId"
-    has_many :comments, class_name: "Comment", foreign_key: "AuthorId"
-    has_many :likes, class_name: "Like", foreign_key: "AuthorId"
+  has_many :posts, class_name: 'Post', foreign_key: 'AuthorId'
+  has_many :comments, class_name: 'Comment', foreign_key: 'AuthorId'
+  has_many :likes, class_name: 'Like', foreign_key: 'AuthorId'
 
-    def recent_post
-        posts.order(create_at: :desc).limit(3)
-    end
+  def recent_post
+    posts.order(create_at: :desc).limit(3)
+  end
 end
-
 
 # Create User
 # user = User.create(Name: 'Tom', Photo: 'https://unsplash.com/photos/F_-0BxGuVvo', Bio: 'Teacher from Mexico.')
