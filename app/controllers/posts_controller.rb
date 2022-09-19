32a912c6a71a042a3bed7de44d0589d452acf784
chrissiku <chrissiku5@gmail.com>
2022-09-19 12:38:21 +0200

    Create `Users` and `Posts` controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def index
+        # My code will be here
+    end
+
+    def show
+        # My code will be here
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    def Index
+        # My code will be here
+    end
+
+    def Show
+        # My code will be here
+    end
+end
