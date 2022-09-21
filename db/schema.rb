5b3efbc18512859e6676be51a663d742ce836894
chrissiku <chrissiku5@gmail.com>
2022-09-21 13:49:22 +0200

    Set default values to `commentsCounter` and `likesCounter`

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_091607) do
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_113635) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -36,8 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_091607) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "commentsCounter"
-    t.integer "likesCounter"
+    t.integer "commentsCounter", default: 0
+    t.integer "likesCounter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "authorId", null: false
@@ -48,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_091607) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "postsCounter"
+    t.integer "postsCounter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
